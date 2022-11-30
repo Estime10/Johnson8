72edc09fdd355d4a8a955c1c743318104761048b
Estime10 <estimevangu.pro@gmail.com>
2022-11-30 12:26:17 +0100

    latest version of psql

---
+++ 2.The Hill/back-end sprint/express/index.mjs
@@ -0,0 +1,33 @@
+import pg from "pg";
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+const client = new pg.Client({
+  user: 'postgres',
+  host: 'localhost',
+  database:"first_db",
+  password: `${process.env.MYSQL_PASSWORD}`,
+  port: 5432,
+})
+
+  const queryTable = `
+  CREATE TABLE usersLists (
+      "id" int,
+      "firstName" varchar not null,
+      "lastName" varchar  not null,
+      "email" varchar not null,
+      "ip" varchar 
+  );
+  `;
+
+client.query(queryTable, (err, res) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log("Table is successfully created");
+  client.end();
+});
+
+client.connect()
+.then(() => console.log("hello there"));
